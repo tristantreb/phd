cd61ccb8d40df5dda20fa74e70a88a1a1f8cf87b
Tristan Trébaol <tristan.trebaol@gmail.com>
2022-07-14 00:33:57 +0100

    added lung model with checks

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ def getMurdererName(val):
     valid = {0,1}
     if val not in valid:
         raise ValueError("results: status must be one of %r." % valid)
-    # use dictionnnary mapping
+    # use dictionnary mapping
     switcher = {
         0: "Grey",
         1: "Auburn",
