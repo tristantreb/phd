b7ae69efb23117e6d6a61055a8b6d1310fc1fb30
Tristan Trébaol <tristan.trebaol@gmail.com>
2022-11-09 12:16:53 +0700

    PLot O2_FEV1 with normalised values

--- normalisations.py
+++ normalisations.py
@@ -1,34 +1,33 @@
 import numpy as np
 
 
-# Normalise by patients' baseline lung function, which is the avg of stable measurements for that patient
-def normalise_by_stable_baseline(O2_FEV1, lung_function_var):
+# Normalise by stable baseline for a given variable
+def normalise_by_stable_baseline(O2_FEV1, var_to_normalise):
     patients_ids = O2_FEV1.ID.unique()
-    O2_FEV1["Norm Lung Function"] = np.nan
+    O2_FEV1["{} norm".format(var_to_normalise)] = np.nan
     for id in patients_ids:
         mask_patient = O2_FEV1.ID == id
-        O2_FEV1["Norm Lung Function"][mask_patient] = normalise_by_patient_stable_baseline(O2_FEV1[mask_patient],
-                                                                                           lung_function_var, id)
+        O2_FEV1["{} norm".format(var_to_normalise)][mask_patient] = normalise_by_patient_stable_baseline(O2_FEV1[mask_patient],
+                                                                                           var_to_normalise, id)
     return O2_FEV1
 
 
-def normalise_by_patient_stable_baseline(patient_O2_FEV1, lung_function_var, id):
-    # filter stable lung function
+# Compute patient stable baseline and normalise by it (mean)
+def normalise_by_patient_stable_baseline(patient_O2_FEV1, var_to_normalise, id):
+    # filter stable periods
     patient_O2_FEV1_stable = patient_O2_FEV1[patient_O2_FEV1["Is Exacerbated"] == False]
-    print(id, len(patient_O2_FEV1_stable))
-    # print(patient_O2_FEV1_stable[['ID', 'FEV1', 'Is Exacerbated', 'Norm Lung Function']])
     if len(patient_O2_FEV1_stable) == 0:
         print("No stable measurements for id {}, filling with NaN".format(id))
         return np.nan
     else:
         # get avg and std of filtered values
-        avg = patient_O2_FEV1_stable[lung_function_var].mean()
-        std = patient_O2_FEV1_stable[lung_function_var].std()
-        print(avg, std)
+        avg = patient_O2_FEV1_stable[var_to_normalise].mean()
+        std = patient_O2_FEV1_stable[var_to_normalise].std()
         match std:
             # normalise by mean
             case np.nan:
-                return patient_O2_FEV1[lung_function_var].apply(lambda x: x - avg)
+                return patient_O2_FEV1[var_to_normalise].apply(lambda x: x - avg)
             # normalise by mean and std dev
             case _:
-                return patient_O2_FEV1[lung_function_var].apply(lambda x: (x - avg) / std)
+                return patient_O2_FEV1[var_to_normalise].apply(lambda x: (x - avg))
+                # return patient_O2_FEV1[var_to_normalise].apply(lambda x: (x - avg) / std)
