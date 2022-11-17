4c27fce5f142221953bf88af3ffe29f2609559bf
Tristan Trébaol <tristan.trebaol@gmail.com>
2022-11-17 11:23:07 +0000

    Add avg(2 highest scores in window) smoothing

--- src/O2-FEV1 analysis/smooth.py
+++ src/O2-FEV1 analysis/smooth.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def smooth_avg(O2_FEV1, var_list, window=3):
     for var in var_list:
         O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window).sum()/3
@@ -5,5 +8,10 @@ def smooth_avg(O2_FEV1, var_list, window=3):
 
 def smooth_max(O2_FEV1, var_list, window=3):
     for var in var_list:
-        O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window).max()
+        O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window, center=True).max()
+    return O2_FEV1
+
+def smooth_avg_of_two_highest(O2_FEV1, var_list, window=3):
+    for var in var_list:
+        O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window, center=True).apply(lambda x: np.sort(x)[-2:].mean())
     return O2_FEV1
