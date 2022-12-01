21ff0d7c0d0ca792e34f21e8380b037ddbbc21a2
Tristan Trébaol <tristan.trebaol@gmail.com>
2022-12-01 01:50:28 +0000

    Make smooth_max smooth per ID instead of globally

--- src/O2-FEV1 analysis/smooth.py
+++ src/O2-FEV1 analysis/smooth.py
@@ -3,15 +3,23 @@ import numpy as np
 
 def smooth_avg(O2_FEV1, var_list, window=3):
     for var in var_list:
-        O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window).sum()/3
+        O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window).sum() / 3
     return O2_FEV1
 
-def smooth_max(O2_FEV1, var_list, window=3):
-    for var in var_list:
-        O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window, center=True).max()
-    return O2_FEV1
 
-def smooth_avg_of_two_highest(O2_FEV1, var_list, window=3):
-    for var in var_list:
-        O2_FEV1["{} smoothed".format(var)] = O2_FEV1[var].rolling(window, center=True).apply(lambda x: np.sort(x)[-2:].mean())
+# Applies a moving average of size 3, then take the max of the 3 values
+def smooth_max(O2_FEV1, var_to_smooth_list, window=3):
+
+    # For each var_to_smooth in var_to_smooth_list, create a column with name "{} smoothed".format(var_to_smooth) filled with nan
+    for var_to_smooth in var_to_smooth_list:
+        O2_FEV1["{} smoothed".format(var_to_smooth)] = np.nan
+
+    # For each each ID in O2_FEV1
+    for id in O2_FEV1["ID"].unique():
+        # Create mask for this ID
+        mask = O2_FEV1["ID"] == id
+        for var_to_smooth in var_to_smooth_list:
+            O2_FEV1["{} smoothed".format(var_to_smooth)][mask] = (
+                O2_FEV1[var_to_smooth][mask].rolling(window, center=True).max()
+            )
     return O2_FEV1
