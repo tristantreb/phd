3a75240c47480425cb02d46a05cb27a3c6c0245f
Tristan Trébaol <tristan.trebaol@gmail.com>
2022-12-11 19:36:21 +0000

    Create df_O2_FEV1

--- src/data/patient_data.py
+++ src/data/patient_data.py
@@ -2,15 +2,15 @@ import pandas as pd
 
 
 def load(datadir):
-    print("** Loading patient data **")
-    patient_data = pd.read_excel(
+    print("\n** Loading patient data **")
+    df = pd.read_excel(
         datadir + "clinicaldata_updated.xlsx", sheet_name="Patients", dtype={"ID": str}
     )
-    n_initial_entries = patient_data.shape[0]
+    n_initial_entries = df.shape[0]
 
     # Drop columns that are not needed
     # List of columns to keep
-    tmp_columns = patient_data.columns
+    tmp_columns = df.columns
     columns_to_keep = [
         "ID",
         "DOB",
@@ -21,16 +21,16 @@ def load(datadir):
         "Predicted FEV1",
         "FEV1 Set As",
     ]
-    patient_data = patient_data[columns_to_keep]
-    print("\n** Dropping unnecessary columns from patient data **")
-    print("Filtering columns: {}".format(columns_to_keep))
+    df = df[columns_to_keep]
+    print("\n* Dropping unnecessary columns from patient data *")
+    print("Columns filtered: {}".format(columns_to_keep))
     print("Columns dropped: {}".format(set(tmp_columns) - set(columns_to_keep)))
 
     # Clean data types
-    patient_data.Weight = patient_data.Weight.replace(to_replace="75,4", value="75.4")
+    df.Weight = df.Weight.replace(to_replace="75,4", value="75.4")
 
     # Enforce data types
-    patient_data = patient_data.astype(
+    df = df.astype(
         {
             "ID": str,
             "Age": int,
@@ -42,28 +42,27 @@ def load(datadir):
         }
     )
     # Cast datetime to date
-    patient_data.DOB = pd.to_datetime(patient_data.DOB).dt.date
+    df.DOB = pd.to_datetime(df.DOB).dt.date
 
     # Correct erroneous data
-    print("\n** Correcting patient data **")
-    patient_data = _correct_patient_data(patient_data)
+    df = _correct_df(df)
 
     # Apply data sanity checks
-    print("\n** Applying data sanity checks **")
-    patient_data.apply(_age_sanity_check, axis=1)
-    patient_data.apply(_sex_sanity_check, axis=1)
-    patient_data.apply(_height_sanity_check, axis=1)
-    patient_data.apply(_weight_sanity_check, axis=1)
+    print("\n* Applying data sanity checks *")
+    df.apply(_age_sanity_check, axis=1)
+    df.apply(_sex_sanity_check, axis=1)
+    df.apply(_height_sanity_check, axis=1)
+    df.apply(_weight_sanity_check, axis=1)
     # Not added because need to decide which PRedicted FEV1 to use
-    # patient_data.apply(_predicted_fev1_sanity_check, axis=1)
-    # patient_data.apply(_fev1_set_as_sanity_check, axis=1)
+    # df.apply(_predicted_fev1_sanity_check, axis=1)
+    # df.apply(_fev1_set_as_sanity_check, axis=1)
 
     print(
         "Loaded patient data with {} entries ({} initially)".format(
-            patient_data.shape[0], n_initial_entries
+            df.shape[0], n_initial_entries
         )
     )
-    return patient_data
+    return df
 
 
 def _age_sanity_check(row):
@@ -84,23 +83,27 @@ def _sex_sanity_check(row):
 def _height_sanity_check(row):
     assert (
         row.Height >= 120 and row.Height <= 220
-    ), "Warning - ID {} has Height ({}) outside 120-220cm range".format(row.ID, row.Height)
+    ), "Warning - ID {} has Height ({}) outside 120-220cm range".format(
+        row.ID, row.Height
+    )
     return -1
 
 
 def _weight_sanity_check(row):
     assert (
         row.Weight >= 30 and row.Weight <= 120
-    ), "Warning - ID {} has Weight ({}) outside 30-120kg range".format(row.ID, row.Weight)
+    ), "Warning - ID {} has Weight ({}) outside 30-120kg range".format(
+        row.ID, row.Weight
+    )
     return -1
 
 
-def _correct_patient_data(patient_data):
-    print("\n** Correcting patient data **")
+def _correct_df(df):
+    print("\n* Correcting patient data *")
 
     # ID 60, convert height from m to cm
-    tmp = patient_data.loc[patient_data.ID == "60", "Height"]
-    patient_data.Height.loc[patient_data.ID == "60"] = tmp * 100
+    tmp = df.loc[df.ID == "60", "Height"]
+    df.Height.loc[df.ID == "60"] = tmp * 100
 
     # Print data correction for ID 60
     print(
@@ -110,8 +113,8 @@ def _correct_patient_data(patient_data):
         )
     )
     # ID 66, convert height from m to cm
-    tmp = patient_data.loc[patient_data.ID == "66", "Height"]
-    patient_data.loc[patient_data.ID == "66", "Height"] = tmp * 100
+    tmp = df.loc[df.ID == "66", "Height"]
+    df.loc[df.ID == "66", "Height"] = tmp * 100
 
     # Print data correction for ID 66
     print(
@@ -120,4 +123,4 @@ def _correct_patient_data(patient_data):
             (tmp * 100).to_string(index=False),
         )
     )
-    return patient_data
+    return df
