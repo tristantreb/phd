10bd646a98e9df71960a28f87538c03404aafacc
Tristan Trébaol <tristan.trebaol@gmail.com>
2022-12-11 17:51:19 +0000

    Add measurements data processing

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from patient_data import *
+
+from .measurements_data import *
+from .patient_data import *
 
 
 def create_O2_FEV1_df(datadir):
@@ -11,18 +13,6 @@ def create_O2_FEV1_df(datadir):
     )
 
     # Load measurements data
-    # Extract data and format datatypes
-    # Don't use convert_dtypes as it provides types that mess up when doing np calculations
-    measurements = pd.read_csv(datadir + "mydata.csv").rename(columns={"FEV 1": "FEV1"})
-    # Cast datetime to date
-    measurements["Date recorded"] = pd.to_datetime(
-        measurements["Date/Time recorded"]
-    ).dt.date
-    # Get ID (same as SmartCare ID)
-    measurements = measurements.merge(
-        id_map, left_on="User ID", right_on="Patient_ID", copy=True
-    )
-    measurements.drop(columns=["User ID", "UserName", "Patient_ID"], inplace=True)
 
     # Load clinical data
     # Patient data: Information describing the patient
@@ -39,44 +29,33 @@ def create_O2_FEV1_df(datadir):
     ).dt.date
     antibioticsdata["Stop Date"] = pd.to_datetime(antibioticsdata["Stop Date"]).dt.date
 
-    # Remove O2 Saturation measurements above 100%
-    len_raw_o2_meas = measurements.shape[0]
-    measurements.drop(
-        measurements[measurements["O2 Saturation"] > 100].index, inplace=True
-    )
-    print(
-        "Removed {} measurements where O2 Sat > 100%, kept {} measurements".format(
-            len_raw_o2_meas - measurements.shape[0], measurements.shape[0]
-        )
-    )
+    # # Get all O2 and FEV1 measurements and merge them
+    # O2 = extract_measure(measurements, "O2 Saturation")
+    # FEV1 = extract_measure(measurements, "FEV1")
+    # len_outer_join = O2.merge(FEV1, on=["ID", "Date recorded"], how="outer").shape[0]
+    # O2_FEV1 = O2.merge(FEV1, on=["ID", "Date recorded"], how="inner")
+    # print(
+    #     "Removed {} rows with O2_FEV1 inner join, kept {:.0%} of measurements ({})".format(
+    #         len_outer_join - O2_FEV1.shape[0],
+    #         O2_FEV1.shape[0] / len_outer_join,
+    #         O2_FEV1.shape[0],
+    #     )
+    # )
 
-    # Get all O2 and FEV1 measurements and merge them
-    O2 = extract_measure(measurements, "O2 Saturation")
-    FEV1 = extract_measure(measurements, "FEV1")
-    len_outer_join = O2.merge(FEV1, on=["ID", "Date recorded"], how="outer").shape[0]
-    O2_FEV1 = O2.merge(FEV1, on=["ID", "Date recorded"], how="inner")
-    print(
-        "Removed {} rows with O2_FEV1 inner join, kept {:.0%} of measurements ({})".format(
-            len_outer_join - O2_FEV1.shape[0],
-            O2_FEV1.shape[0] / len_outer_join,
-            O2_FEV1.shape[0],
-        )
-    )
+    # # Remove duplicates
+    # # TODO: take last or average? How to best handle duplicates?
+    # # Remove duplicated measurements
+    # len_all_fev1_o2 = O2_FEV1.shape[0]
+    # O2_FEV1 = O2_FEV1[
+    #     O2_FEV1.duplicated(subset=["ID", "Date recorded"], keep="last") == False
+    # ]
+    # print(
+    #     "Removed {} duplicates, {} measurements left".format(
+    #         len_all_fev1_o2 - O2_FEV1.shape[0], O2_FEV1.shape[0]
+    #     )
+    # )
 
-    # Remove duplicates
-    # TODO: take last or average? How to best handle duplicates?
-    # Remove duplicated measurements
-    len_all_fev1_o2 = O2_FEV1.shape[0]
-    O2_FEV1 = O2_FEV1[
-        O2_FEV1.duplicated(subset=["ID", "Date recorded"], keep="last") == False
-    ]
-    print(
-        "Removed {} duplicates, {} measurements left".format(
-            len_all_fev1_o2 - O2_FEV1.shape[0], O2_FEV1.shape[0]
-        )
-    )
-
-    return O2_FEV1
+    return -1
 
 
 # Function to extract one column from the data
--- src/data/patient_data.py
+++ src/data/patient_data.py
@@ -2,15 +2,15 @@ import pandas as pd
 
 
 def load(datadir):
+    print("** Loading patient data **")
     patient_data = pd.read_excel(
         datadir + "clinicaldata_updated.xlsx", sheet_name="Patients", dtype={"ID": str}
     )
-    print("** Loading patient data **")
-    n_initial_entries = patient_data.shape[0]))
+    n_initial_entries = patient_data.shape[0]
 
     # Drop columns that are not needed
     # List of columns to keep
-    columns = patient_data.columns
+    tmp_columns = patient_data.columns
     columns_to_keep = [
         "ID",
         "DOB",
@@ -24,12 +24,12 @@ def load(datadir):
     patient_data = patient_data[columns_to_keep]
     print("\n** Dropping unnecessary columns from patient data **")
     print("Filtering columns: {}".format(columns_to_keep))
-    print("Columns dropped: {}".format(set(columns) - set(columns_to_keep)))
+    print("Columns dropped: {}".format(set(tmp_columns) - set(columns_to_keep)))
 
     # Clean data types
     patient_data.Weight = patient_data.Weight.replace(to_replace="75,4", value="75.4")
 
-    # Enforce the data types
+    # Enforce data types
     patient_data = patient_data.astype(
         {
             "ID": str,
@@ -41,12 +41,15 @@ def load(datadir):
             "FEV1 Set As": float,
         }
     )
+    # Cast datetime to date
     patient_data.DOB = pd.to_datetime(patient_data.DOB).dt.date
 
-    # Clean data
+    # Correct erroneous data
+    print("\n** Correcting patient data **")
     patient_data = _correct_patient_data(patient_data)
 
-    # Data sanity checks
+    # Apply data sanity checks
+    print("\n** Applying data sanity checks **")
     patient_data.apply(_age_sanity_check, axis=1)
     patient_data.apply(_sex_sanity_check, axis=1)
     patient_data.apply(_height_sanity_check, axis=1)
@@ -55,14 +58,18 @@ def load(datadir):
     # patient_data.apply(_predicted_fev1_sanity_check, axis=1)
     # patient_data.apply(_fev1_set_as_sanity_check, axis=1)
 
-    print("Loaded patient data with {} entries ({} initially)".format(patient_data.shape[0], n_initial_entries))
+    print(
+        "Loaded patient data with {} entries ({} initially)".format(
+            patient_data.shape[0], n_initial_entries
+        )
+    )
     return patient_data
 
 
 def _age_sanity_check(row):
     assert (
         row.Age >= 18 and row.Age <= 70
-    ), "Age ({}) outside 18-70 range for ID {}".format(row.Age, row.ID)
+    ), "Warning - ID {} has Age ({}) outside 18-70 range".format(row.ID, row.Age)
     return -1
 
 
@@ -70,19 +77,21 @@ def _sex_sanity_check(row):
     assert row.Sex in [
         "Male",
         "Female",
-    ], "Sex ({}) is not 'Male' neither 'Female' for ID {}".format(row.Sex, row.ID)
+    ], "Sex ({}) is not 'Male' neither 'Female'".format(row.ID, row.Sex)
     return -1
+
 
 def _height_sanity_check(row):
     assert (
         row.Height >= 120 and row.Height <= 220
-    ), "Height ({}) outside 120-220cm range for ID {}".format(row.Height, row.ID)
+    ), "Warning - ID {} has Height ({}) outside 120-220cm range".format(row.ID, row.Height)
     return -1
+
 
 def _weight_sanity_check(row):
     assert (
         row.Weight >= 30 and row.Weight <= 120
-    ), "Weight ({}) outside 30-120kg range for ID {}".format(row.Weight, row.ID)
+    ), "Warning - ID {} has Weight ({}) outside 30-120kg range".format(row.ID, row.Weight)
     return -1
 
 
@@ -95,7 +104,7 @@ def _correct_patient_data(patient_data):
 
     # Print data correction for ID 60
     print(
-        "Corrected height for ID 60 from {} to {}".format(
+        "ID 60: Corrected height 60 from {} to {}".format(
             (tmp).to_string(index=False),
             (tmp * 100).to_string(index=False),
         )
@@ -106,7 +115,7 @@ def _correct_patient_data(patient_data):
 
     # Print data correction for ID 66
     print(
-        "Corrected height for ID 66 from {} to {}".format(
+        "ID 66: Corrected height for ID 66 from {} to {}".format(
             (tmp).to_string(index=False),
             (tmp * 100).to_string(index=False),
         )
