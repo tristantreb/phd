55be47711b856496312be223b5a39cac7c466350
Tristan Trébaol <tristan.trebaol@gmail.com>
2022-12-12 16:02:39 +0000

    Finish choose the right predicted FEV1 exploration

--- src/data/biology.py
+++ src/data/biology.py
@@ -1,7 +1,20 @@
-def calc_predicted_fev1(row):
+def calc_predicted_fev1_from_row(row):
     # Calculate predicted FEV1 based on the formula from Andres
-    # The input should be a df row with FEV1, Age, Height, Sex
+    # This formula takes Age in years, Height in m and Sex
+    # Input Height is in cm, hence the /100
+
     if row.Sex == "Male":
-        return row.Height * 0.01 * 4.3 - row.Age * 0.029 - 2.49
+        return (row.Height / 100) * 4.3 - row.Age * 0.029 - 2.49
     elif row.Sex == "Female":
-        return row.Height * 0.01 * 3.95 - row.Age * 0.025 - 2.6
+        return (row.Height / 100) * 3.95 - row.Age * 0.025 - 2.6
+
+
+def calc_predicted_fev1(height, age, sex):
+    # Calculate predicted FEV1 based on the formula from Andres
+    # This formula takes Age in years, Height in m and Sex
+    # Input Height is in cm, hence the /100
+
+    if sex == "Male":
+        return (height / 100) * 4.3 - age * 0.029 - 2.49
+    elif sex == "Female":
+        return (height / 100) * 3.95 - age * 0.025 - 2.6
