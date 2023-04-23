5e6e22a826b9d72bfafee68e33fdbb32a4d5d719
Tristan Trébaol <tristan.trebaol@gmail.com>
2023-04-23 13:02:12 +0100

    2 fixes in the variableNode class + update tree

--- src/milestone_model/interactive_inference.py
+++ src/milestone_model/interactive_inference.py
@@ -1,17 +1,18 @@
-import bp
-import plotly.express as px
-from dash import Dash, Input, Output, dcc, html
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import numpy as np
-
-
-from lung_health_model import FEV1, C, U, inference
-
-# Solving: "Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized" error
-# https://stackoverflow.com/questions/53014306/error-15-initializing-libiomp5-dylib-but-found-libiomp5-dylib-already-initial
 import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+import model_helpers as mh
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import Dash, Input, Output, dcc, html
+from model_lung_health import FEV1, C, U, inference
+from plotly.subplots import make_subplots
+
+"""
+Solving: "Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized" error
+https://stackoverflow.com/questions/53014306/error-15-initializing-libiomp5-dylib-but-found-libiomp5-dylib-already-initial
+"""
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 
 app = Dash(__name__)
 
@@ -30,11 +31,12 @@ app.layout = html.Div(
     ]
 )
 
+
 @app.callback(Output("graph", "figure"), Input("fev1", "value"))
 def display(fev1):
     print("user input: FEV1 set to", fev1)
 
-    [_fev1_bin, fev1_idx] = bp.get_bin_for_value(fev1, FEV1.bins)
+    [_fev1_bin, fev1_idx] = mh.get_bin_for_value(fev1, FEV1.bins)
 
     res_u = inference.query(variables=[U.name], evidence={FEV1.name: fev1_idx})
     res_c = inference.query(variables=[C.name], evidence={FEV1.name: fev1_idx})
@@ -43,15 +45,16 @@ def display(fev1):
 
     fig = make_subplots(rows=1, cols=2)
     fig.add_trace(go.Bar(y=res_u.values, x=U.bins[:-1]), row=1, col=1)
-    fig['layout']['xaxis']['title']=U.name
+    fig["layout"]["xaxis"]["title"] = U.name
 
     fig.add_trace(go.Bar(y=res_c.values, x=C.bins[:-1]), row=1, col=2)
-    fig['layout']['xaxis2']['title']=C.name
+    fig["layout"]["xaxis2"]["title"] = C.name
     # intbins=(C.bins*100).astype(int)
     # shortbins=np.delete(intbins, np.arange(1,intbins.size,2))
     # fig.update_layout(xaxis2=dict(tickvals=C.bins.tolist()))
 
     fig.update_layout(showlegend=False)
     return fig
+
 
 app.run_server(debug=True, port=8049, use_reloader=False)
--- src/milestone_model/model_lung_health.py
+++ src/milestone_model/model_lung_health.py
@@ -1,18 +1,18 @@
-import bp
+import model_helpers as mh
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import BeliefPropagation
 from pgmpy.models import BayesianNetwork
 
-U = bp.variableNode("Unblocked FEV1 (L)", 2, 6, 0.1)
-C = bp.variableNode("1-Small airway blockage (%)", 0.1, 1, 0.05)
-FEV1 = bp.variableNode("FEV1 (L)", 0.2, 6, 0.1)
+U = mh.variableNode("Unblocked FEV1 (L)", 2, 6, 0.1)
+C = mh.variableNode("Small airway availability (%)", 0.1, 1, 0.05)
+FEV1 = mh.variableNode("FEV1 (L)", 0.2, 6, 0.1)
 
 graph = BayesianNetwork([(U.name, FEV1.name), (C.name, FEV1.name)])
 
 cpt_fev1 = TabularCPD(
     variable=FEV1.name,
     variable_card=len(FEV1.bins) - 1,
-    values=bp.calc_pgmpy_cpt(U, C, FEV1),
+    values=mh.calc_pgmpy_cpt(U, C, FEV1),
     evidence=[C.name, U.name],
     evidence_card=[len(C.bins) - 1, len(U.bins) - 1],
 )
@@ -20,7 +20,7 @@ cpt_fev1 = TabularCPD(
 prior_b = TabularCPD(
     variable=C.name,
     variable_card=len(C.bins) - 1,
-    values=C.marginal(C),
+    values=C.uniform_prior(C),
     evidence=[],
     evidence_card=[],
 )
@@ -28,7 +28,7 @@ prior_b = TabularCPD(
 prior_u = TabularCPD(
     variable=U.name,
     variable_card=len(U.bins) - 1,
-    values=U.marginal(U),
+    values=U.uniform_prior(U),
     evidence=[],
     evidence_card=[],
 )
