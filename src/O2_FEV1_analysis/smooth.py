da0adf5bae28e8198ee30aa541d945a44fc083d4
Tristan Trébaol <tristan.trebaol@gmail.com>
2023-11-09 11:26:11 +0100

    Fix smooth.smooth_vector start idx

--- src/O2_FEV1_analysis/smooth.py
+++ src/O2_FEV1_analysis/smooth.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 
 
-# Smooth the requested columns of O2-FEV1 with smooth_func
 def smooth(O2_FEV1, var_list, mode="max"):
+    """
+    Smooth the requested columns of O2-FEV1 with smooth_func
+    """
     # Sort O2_FEV1 by ID and Date Recorded
     O2_FEV1 = O2_FEV1.sort_values(by=["ID", "Date Recorded"])
 
@@ -20,16 +22,19 @@ def smooth(O2_FEV1, var_list, mode="max"):
     return O2_FEV1
 
 
-# Applies a moving max/mean of size 3 on the input vector
 def smooth_vector(vector, mode):
+    """
+    Applies a moving max/mean of size 3 on the input vector
+    """
+    print(vector)
     # Copy the vector
     smoothed_vector = vector.copy()
     # Get the vector length
     n = len(vector)
     # For i going from 1 to vector length
     for i in range(0, n - 1):
-        # If i == 1, set the value to the max between the current value and the next two values
-        if i == 1:
+        # If i == 0, set the value to the max between the current value and the next two values
+        if i == 0:
             smoothed_vector[i] = eval("np." + mode)(
                 [vector[i], vector[i + 1], vector[i + 2]]
             )
