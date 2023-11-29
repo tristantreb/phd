adc54a26be58b2b3facbc52157941f8a351b6442
Tristan Trébaol <tristan.trebaol@gmail.com>
2023-11-29 11:26:10 +0000

    Add O2 side to interactive point in time model

--- src/inference/o2_fev1_point_in_time_inference.py
+++ src/inference/o2_fev1_point_in_time_inference.py
@@ -3,15 +3,13 @@ import os
 
 import dash_bootstrap_components as dbc
 import numpy as np
-import plotly.express as px
 import plotly.graph_objects as go
 from dash import Dash, Input, Output, dcc, html
 from plotly.subplots import make_subplots
 
 import src.inference.helpers as ih
-import src.modelling_fev1.pred_fev1 as pred_fev1
-import src.models.builders as mb
 import src.models.helpers as mh
+import src.models.o2_fev1_point_in_time as model
 
 """
 Solving: "Error #15: Initializing libiomp5.dylib, but found libiomp5.dylib already initialized" error
@@ -27,10 +25,10 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.UNITED])
 
 app.layout = dbc.Container(
     [
-        html.H2("Lung Health's Tool", style={"textAlign": "center"}),
+        html.H2("Lung health of an individual", style={"textAlign": "center"}),
         html.Div(
             [
-                html.H4("Individual's clinical profile:"),
+                # html.H4("Individual's clinical profile:"),
                 dbc.InputGroup(
                     [
                         dbc.InputGroupText("Sex"),
@@ -81,8 +79,8 @@ app.layout = dbc.Container(
                 dcc.Store(id="HFEV1", storage_type="session"),
                 dcc.Store(id="FEV1", storage_type="session"),
                 dcc.Store(id="AR", storage_type="session"),
-                # dcc.Store(id='HO2Sat', storage_type='session')
-                # dcc.Store(id='O2SatFFA', storage_type='session')
+                dcc.Store(id="HO2Sat", storage_type="session"),
+                dcc.Store(id="O2SatFFA", storage_type="session"),
             ],
         ),
         html.Div(
@@ -117,14 +115,14 @@ app.layout = dbc.Container(
     Output("HFEV1", "data"),
     Output("FEV1", "data"),
     Output("AR", "data"),
-    # Output("HO2Sat", "data"),
-    # Output("O2SatFFA", "data"),
+    Output("HO2Sat", "data"),
+    Output("O2SatFFA", "data"),
     # Inputs
     Input("sex-select", "value"),
     Input("age-input", "value"),
     Input("height-input", "value"),
 )
-def compute_cpts(sex: str, age: int, height: int):
+def calc_cpts(sex: str, age: int, height: int):
     # TODO: why not int by default?
     height = int(height)
     age = int(age)
@@ -138,16 +136,18 @@ def compute_cpts(sex: str, age: int, height: int):
         HFEV1,
         FEV1,
         AR,
-    ) = mb.calc_point_in_time_cpts(hfev1_prior, ho2sat_prior)
+        HO2Sat,
+        O2SatFFA,
+    ) = model.calc_cpts(hfev1_prior, ho2sat_prior)
 
     # Encode variables
     HFEV1 = mh.encode_node_variable(HFEV1)
     FEV1 = mh.encode_node_variable(FEV1)
-    # HO2Sat= mh.encode_node_variable(HO2Sat)
-    # O2SatFFA= mh.encode_node_variable(O2SatFFA)
     AR = mh.encode_node_variable(AR)
+    HO2Sat = mh.encode_node_variable(HO2Sat)
+    O2SatFFA = mh.encode_node_variable(O2SatFFA)
 
-    return HFEV1, FEV1, AR
+    return HFEV1, FEV1, AR, HO2Sat, O2SatFFA
 
 
 @app.callback(
@@ -158,90 +158,103 @@ def compute_cpts(sex: str, age: int, height: int):
     Input("HFEV1", "data"),
     Input("FEV1", "data"),
     Input("AR", "data"),
-    # Input("HO2Sat", "data"),
-    # Input("O2SatFFA", "data"),
+    Input("HO2Sat", "data"),
+    Input("O2SatFFA", "data"),
     # Evidences
     Input("FEV1-slider", "value"),
 )
-def model_and_inference(HFEV1, FEV1, AR, FEV1_obs: float):
+def model_and_inference(HFEV1, ecFEV1, AR, HO2Sat, O2SatFFA, FEV1_obs: float):
     """
     Decodes inputs from JSON format, build model, runs inference, and returns a figure
     """
     # Decode input vars
     HFEV1 = mh.decode_node_variable(HFEV1)
-    FEV1 = mh.decode_node_variable(FEV1)
+    ecFEV1 = mh.decode_node_variable(ecFEV1)
     AR = mh.decode_node_variable(AR)
-    # HO2Sat = mh.decode_node_variable(HO2Sat)
-    # O2SatFFA = mh.decode_node_variable(O2SatFFA)
+    HO2Sat = mh.decode_node_variable(HO2Sat)
+    O2SatFFA = mh.decode_node_variable(O2SatFFA)
 
     # Build model
-    _, inf_alg = mb.build_point_in_time_pgmpy_model(HFEV1, FEV1, AR)
+    _, inf_alg = model.build_pgmpy_model(HFEV1, ecFEV1, AR, HO2Sat, O2SatFFA)
 
     # INFERENCE
     print("Inference user input: FEV1 set to", FEV1_obs)
 
-    res_hfev1 = ih.infer(inf_alg, [HFEV1], [[FEV1, FEV1_obs]])
-    res_ar = ih.infer(inf_alg, [AR], [[FEV1, FEV1_obs]])
-    # res_ho2sat = ih.infer(inf_alg, [HO2Sat], [[FEV1, FEV1_obs]])
-    # res_o2satffa = ih.infer(inf_alg, [O2SatFFA], [[FEV1, FEV1_obs]])
+    res_hfev1 = ih.infer(inf_alg, [HFEV1], [[ecFEV1, FEV1_obs]])
+    res_ar = ih.infer(inf_alg, [AR], [[ecFEV1, FEV1_obs]])
+    res_ho2sat = ih.infer(inf_alg, [HO2Sat], [[ecFEV1, FEV1_obs]])
+    res_o2satffa = ih.infer(inf_alg, [O2SatFFA], [[ecFEV1, FEV1_obs]])
 
     # PLOT
-    n_var_rows = 3
+    # Priors take 1x1 cells, posteriors take 2x2 cells
     prior = {"type": "bar"}
     posterior = {"type": "bar", "rowspan": 2, "colspan": 2}
 
+    viz_layout = [
+        [prior, None, None, None, prior, None],
+        [posterior, None, None, None, posterior, None],
+        [None, None, None, None, None, None],
+        [None, None, prior, None, None, None],
+        [None, None, posterior, None, None, None],
+        [None, None, None, None, None, None],
+        [None, None, None, None, posterior, None],
+        [None, None, None, None, None, None],
+    ]
+
     fig = make_subplots(
-        # Prior takes a 1x1 cell, Posterior takes a 2x2 cell
-        rows=n_var_rows * 2,
-        cols=6,
-        specs=[
-            [prior, None, None, None, None, None],
-            [posterior, None, None, None, None, None],
-            [None, None, None, None, None, None],
-            [None, None, prior, None, None, None],
-            [None, None, posterior, None, None, None],
-            [None, None, None, None, None, None],
-            # [None, None, None, None, posterior, None],
-            # [None, None, None, None, None, None],
-            # [None, None, None, None, None, None],
-        ],
+        rows=np.shape(viz_layout)[0], cols=np.shape(viz_layout)[1], specs=viz_layout
     )
+
+    fev1_min = 0
+    fev1_max = 6
+    o2sat_min = 60
+    o2sat_max = 100
 
     # HFEV1
     fig.add_trace(go.Bar(y=HFEV1.prior[:, 0], x=HFEV1.bins), row=1, col=1)
     fig["data"][0]["marker"]["color"] = "black"
+    fig.update_xaxes(
+        range=[fev1_min, fev1_max],
+        row=1,
+        col=1,
+    )
     # fig.update_xaxes(title_text="Prior for " + HFEV1.name, row=1, col=1)
 
     fig.add_trace(go.Bar(y=res_hfev1.values, x=HFEV1.bins), row=2, col=1)
     fig["data"][1]["marker"]["color"] = "black"
-    fig.update_xaxes(title_text=HFEV1.name, row=2, col=1)
+    fig.update_xaxes(title_text=HFEV1.name, range=[fev1_min, fev1_max], row=2, col=1)
 
     # HO2Sat
-    # fig.add_trace(go.Bar(y=HO2Sat.prior[:, 0], x=HO2Sat.bins), row=1, col=5)
-    # fig["data"][2]["marker"]["color"] = "blue"
-    # # fig.update_xaxes(title_text="Prior for " + HO2Sat.name, row=1, col=5)
+    fig.add_trace(go.Bar(y=HO2Sat.prior[:, 0], x=HO2Sat.bins), row=1, col=5)
+    fig["data"][2]["marker"]["color"] = "blue"
+    fig.update_xaxes(range=[o2sat_min, o2sat_max], row=1, col=5)
+    # fig.update_xaxes(title_text="Prior for " + HO2Sat.name, row=1, col=5)
 
-    # fig.add_trace(go.Bar(y=res_ho2sat.values, x=HO2Sat.bins), row=2, col=5)
-    # fig["data"][3]["marker"]["color"] = "blue"
-    # fig.update_xaxes(title_text=HO2Sat.name, row=2, col=5)
+    fig.add_trace(go.Bar(y=res_ho2sat.values, x=HO2Sat.bins), row=2, col=5)
+    fig["data"][3]["marker"]["color"] = "blue"
+    fig.update_xaxes(title_text=HO2Sat.name, range=[o2sat_min, o2sat_max], row=2, col=5)
 
     # AR
     fig.add_trace(go.Bar(y=AR.prior[:, 0], x=AR.bins), row=4, col=3)
-    fig["data"][2]["marker"]["color"] = "green"
+    fig["data"][4]["marker"]["color"] = "green"
     # fig.update_xaxes(title_text="Prior for " + AR.name, row=4, col=3)
 
     fig.add_trace(go.Bar(y=res_ar.values, x=AR.bins), row=5, col=3)
-    fig["data"][3]["marker"]["color"] = "green"
+    fig["data"][5]["marker"]["color"] = "green"
     fig.update_xaxes(title_text=AR.name, row=5, col=3)
 
     # O2SatFFA
-    # fig.add_trace(go.Bar(y=res_o2satffa.values, x=O2SatFFA.bins), row=7, col=5)
-    # fig["data"][6]["marker"]["color"] = "cyan"
-    # fig.update_xaxes(title_text=O2SatFFA.name, row=7, col=5)
+    fig.add_trace(go.Bar(y=res_o2satffa.values, x=O2SatFFA.bins), row=7, col=5)
+    fig["data"][6]["marker"]["color"] = "cyan"
+    fig.update_xaxes(
+        title_text=O2SatFFA.name, range=[o2sat_min, o2sat_max], row=7, col=5
+    )
 
-    fig.update_layout(showlegend=False, height=800, width=1400, font=dict(size=8))
+    fig.update_layout(
+        showlegend=False, height=800, width=1400, font=dict(size=10), bargap=0.01
+    )
 
-    return fig, FEV1.a, FEV1.b
+    return fig, ecFEV1.a, ecFEV1.b
 
 
 app.run_server(debug=True, port=8051, use_reloader=False)
---
+++ src/models/o2_fev1_point_in_time.py
@@ -0,0 +1,90 @@
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import BeliefPropagation
+from pgmpy.models import BayesianNetwork
+
+import src.modelling_o2.o2satffa as o2satffa
+import src.models.helpers as mh
+
+
+def build(hfev1_prior, ho2sat_prior):
+    """
+    This is a point in time model with:
+    FEV1 = HFEV1 * (1-AR)
+    O2SatFFA = HO2Sat * drop_func(AR)
+    """
+    vars = calc_cpts(hfev1_prior, ho2sat_prior)
+    model, inf_alg = build_pgmpy_model(*vars)
+    return model, inf_alg, *vars
+
+
+def calc_cpts(hfev1_prior, ho2sat_prior):
+    # Build variables
+    HFEV1 = mh.variableNode("Healthy FEV1 (L)", 1, 6, 0.05, prior=hfev1_prior)
+    ecFEV1 = mh.variableNode("ecFEV1 (L)", 0, 6, 0.05, prior=None)
+    AR = mh.variableNode("Airway Resistance (%)", 0, 90, 2, prior={"type": "uniform"})
+    HO2Sat = mh.variableNode(
+        "Healthy O2 Saturation (%)", 90, 100, 1, prior=ho2sat_prior
+    )
+    O2SatFFA = mh.variableNode(
+        "O2 Sat if fully functional alveoli (%)", 60, 100, 1, prior=None
+    )
+
+    # Calculate CPTs
+    ecFEV1.prior = mh.calc_pgmpy_cpt_X_x_1_minus_Y(HFEV1, AR, ecFEV1)
+    O2SatFFA.prior = o2satffa.calc_cpt(O2SatFFA, HO2Sat, AR, debug=False)
+
+    return (HFEV1, ecFEV1, AR, HO2Sat, O2SatFFA)
+
+
+def build_pgmpy_model(HFEV1, ecFEV1, AR, HO2Sat, O2SatFFA):
+    prior_hfev1 = TabularCPD(
+        variable=HFEV1.name,
+        variable_card=len(HFEV1.bins),
+        values=HFEV1.prior,
+        evidence=[],
+        evidence_card=[],
+    )
+    cpt_ecfev1 = TabularCPD(
+        variable=ecFEV1.name,
+        variable_card=len(ecFEV1.bins),
+        values=ecFEV1.prior,
+        evidence=[HFEV1.name, AR.name],
+        evidence_card=[len(HFEV1.bins), len(AR.bins)],
+    )
+
+    prior_ar = TabularCPD(
+        variable=AR.name,
+        variable_card=len(AR.bins),
+        values=AR.prior,
+        evidence=[],
+        evidence_card=[],
+    )
+    prior_ho2sat = TabularCPD(
+        variable=HO2Sat.name,
+        variable_card=len(HO2Sat.bins),
+        values=HO2Sat.prior,
+        evidence=[],
+        evidence_card=[],
+    )
+    cpt_o2satffa = TabularCPD(
+        variable=O2SatFFA.name,
+        variable_card=len(O2SatFFA.bins),
+        values=O2SatFFA.prior,
+        evidence=[HO2Sat.name, AR.name],
+        evidence_card=[len(HO2Sat.bins), len(AR.bins)],
+    )
+
+    model = BayesianNetwork(
+        [
+            (HFEV1.name, ecFEV1.name),
+            (AR.name, ecFEV1.name),
+            (HO2Sat.name, O2SatFFA.name),
+            (AR.name, O2SatFFA.name),
+        ]
+    )
+
+    model.add_cpds(cpt_ecfev1, prior_ar, prior_hfev1, prior_ho2sat, cpt_o2satffa)
+
+    model.check_model()
+    inf_alg = BeliefPropagation(model)
+    return (model, inf_alg)
