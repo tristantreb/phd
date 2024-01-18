79da0337731d3dabe5aed8a537ad75e7cbd49ad0
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-01-18 16:39:25 +0000

    Run a profiler and speedup inference

--- src/inference/helpers.py
+++ src/inference/helpers.py
@@ -15,7 +15,7 @@ def infer(
     variables: tuple[mh.variableNode],
     evidences: tuple[tuple[mh.variableNode, float]],
     show_progress=False,
-    joint=True,
+    joint=False,
 ):
     """
     Runs an inference query against a given PGMPY inference model, variables, evidences
@@ -32,14 +32,12 @@ def infer(
         [_bin, bin_idx] = get_bin_for_value(value, evidence_var)
         evidences_binned.update({evidence_var.name: bin_idx})
 
-    # tic = time.time()
     query = inference_alg.query(
         variables=var_names,
         evidence=evidences_binned,
         show_progress=show_progress,
         joint=joint,
     )
-    # print(f"Query took {time.time() - tic}s")
 
     return query
 
