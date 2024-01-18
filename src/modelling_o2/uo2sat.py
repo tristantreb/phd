d80b799b0c26b459120bbf402e651822f596f0ac
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-01-18 18:48:53 +0000

    Run the profiler again

--- src/modelling_o2/uo2sat.py
+++ src/modelling_o2/uo2sat.py
@@ -4,6 +4,8 @@ import src.models.helpers as mh
 def calc_cpt(UO2Sat: mh.variableNode, O2SatFFA: mh.variableNode, IA: mh.variableNode):
     """
     UO2Sat = O2SatFFA * (1-IA)
-    Unbiased O2 saturation
+    UO2Sat: Underling O2 saturation
+    O2SatFFA: O2 saturation if fully functional alveoli
+    IA: Inactive alveoli
     """
     return mh.calc_pgmpy_cpt_X_x_1_minus_Y(O2SatFFA, IA, UO2Sat)
