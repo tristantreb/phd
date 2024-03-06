d7cef7d37c1941f2168e2aff1a570d76955d8fdb
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-03-06 17:54:56 +0000

    Rewrite slicing function with an automatic convergence criteria

--- src/inference/long_inf_slicing.py
+++ src/inference/long_inf_slicing.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from typing import List
 
 import numpy as np
 import pandas as pd
@@ -30,3 +31,95 @@ class SharedNodeVariable:
             return list(messages_up.values())[0]
         else:
             return reduce(np.multiply, list(messages_up.values()))
+
+
+def get_diffs(res, posteriors_old, vars):
+    diffs = []
+    posteriors_new = []
+    for var, old in zip(vars, posteriors_old):
+        new, diff = get_diff(res, old, var)
+        diffs.append(diff)
+        posteriors_new.append(new)
+    return posteriors_new, diffs
+
+
+def get_diff(res, old, var):
+    new = res[var.name].values
+    diff = np.abs(new - old).sum()
+    return new, diff
+
+
+def get_uniform_message(card):
+    return np.ones(card) / card
+
+
+def query_across_days(
+    df,
+    belief_propagation,
+    shared_variables: List[SharedNodeVariable],
+    variables: List[str],
+):
+    epoch = 0
+    df_res = pd.DataFrame(
+        columns=["Epoch"] + list(map(lambda v: v.name, shared_variables))
+    )
+
+    # Initialize posteriors distribution to uniform
+    posteriors_old = [
+        get_uniform_message(shared_var.card) for shared_var in shared_variables
+    ]
+
+    while True:
+        print(f"epoch {epoch}")
+
+        for i in range(len(df)):
+            day = df["Date Recorded"].iloc[i].strftime("%Y-%m-%d")
+
+            def build_evidence(variables):
+                evidence = {}
+                for variable in variables:
+                    idx_obs = df[variable].iloc[i]
+                    evidence[variable] = idx_obs
+                return evidence
+
+            evidence = build_evidence(variables)
+
+            def build_virtual_evidence(shared_variables):
+                virtual_evidence = {}
+                for shared_var in shared_variables:
+                    virtual_message = shared_var.get_virtual_message(day)
+                    if virtual_message is not None:
+                        virtual_evidence[shared_var.name] = virtual_message
+                return virtual_evidence
+
+            virtual_evidence = build_virtual_evidence(shared_variables)
+
+            var_to_infer = list(map(lambda v: v.name, shared_variables))
+
+            # Query the graph
+            res, messages = belief_propagation.query(
+                var_to_infer, evidence, virtual_evidence, get_messages=True
+            )
+
+            # Save message for current day
+            for shared_var in shared_variables:
+                shared_var.add_message(day, messages[shared_var.graph_key])
+
+        posteriors_old, diffs = get_diffs(res, posteriors_old, shared_variables)
+
+        for shared_var, diff in zip(shared_variables, diffs):
+            print(f"Epoch {epoch} - Posteriors' diff for {shared_var.name}: {diff}")
+
+        # Create new row df with epoch, and on shared variables array per row cel
+        new_row = [epoch] + list(map(lambda v: res[v.name].values, shared_variables))
+        # Same but as df
+        new_row = pd.DataFrame(
+            [new_row], columns=["Epoch"] + list(map(lambda v: v.name, shared_variables))
+        )
+
+        df_res = pd.concat([df_res, new_row], ignore_index=True)
+
+        if np.sum(diffs) == 0:
+            print("All diffs are 0, inference converged")
+            return df_res
+        epoch += 1
