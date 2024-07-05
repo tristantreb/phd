c569f586da9958667992051601abc6589a9b14ec
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-07-05 12:29:58 +0100

    Export plot_2d_cpt function to cpts.helpers folder

---
+++ src/models/cpts/helpers.py
@@ -0,0 +1,87 @@
+from typing import List
+
+import numpy as np
+from plotly.subplots import make_subplots
+
+import src.data.helpers as dh
+import src.inference.helpers as ih
+import src.models.helpers as mh
+from src.models.helpers import name_to_abbr
+
+
+def get_cpt(vars: List[mh.VariableNode], suffix=None):
+    path_to_folder = dh.get_path_to_src() + "models/cpts/"
+    var_spec = map(
+        lambda var: f"{name_to_abbr(var.name)}_{var.a}_{var.b}_{var.bin_width}", vars
+    )
+    filename = "_".join(var_spec)
+
+    if suffix is not None:
+        filename = filename + suffix
+
+    cpt = np.load(f"{path_to_folder}{filename}.npy")
+
+    assert len(vars) == len(cpt.shape)
+    return cpt
+
+
+def save_cpt(vars: List[mh.VariableNode], cpt: np.ndarray, suffix=None):
+    path_to_folder = dh.get_path_to_src() + "/models/cpts/"
+    filename = "_".join(
+        [f"{name_to_abbr(var.name)}_{var.a}_{var.b}_{var.bin_width}" for var in vars]
+    )
+
+    if suffix is not None:
+        filename = filename + suffix
+
+    assert len(vars) == len(cpt.shape)
+
+    np.save(
+        f"{path_to_folder}{filename}",
+        cpt,
+    )
+    return
+
+
+def plot_2d_cpt(cpt, cVar, pVar, height=2500, vspace=0.003, invert=False):
+    """
+    CPT represents P(cVar | pVar)
+    The probability distribution of the child variable conditionned on the parent variable
+    """
+    if invert:
+        cpt = cpt.T
+        vartmp = cVar
+        cVar = pVar
+        pVar = vartmp
+
+    plot_bins = range(pVar.card)
+    title = f"CPT - P({cVar.get_abbr()}|{pVar.get_abbr()})"
+
+    fig = make_subplots(
+        rows=len(plot_bins), cols=1, shared_xaxes=True, vertical_spacing=vspace
+    )
+
+    for i, idx in enumerate(plot_bins):
+        p = cpt[:, idx]
+        ih.plot_histogram(
+            fig, cVar, p, cVar.a, cVar.b, i + 1, 1, colour="#0072b2", annot=False
+        )
+        fig.update_yaxes(
+            title_text=f"{pVar.get_abbr()}={pVar.midbins[idx]:2g}",
+            row=i + 1,
+            col=1,
+            range=[0, 0.6],
+        )
+
+    fig.update_layout(
+        title=title,
+        width=500,
+        height=height,
+        showlegend=False,
+        font=dict(size=8),
+    )
+    fig.show()
+    # fig.write_image(
+    #     dh.get_path_to_main()
+    #     + f"PlotsBreathe/AR_modelling/{title}.pdf"
+    # )
