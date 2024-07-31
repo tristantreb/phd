28b140e3e1c395f0d2c2b0142903e1e8014b6437
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-07-31 19:12:50 +0200

    Introduce back and forth slicing algorithm, validate it against forward only algorithm

--- src/inference/long_inf_slicing.py
+++ src/inference/long_inf_slicing.py
@@ -85,17 +85,19 @@ def build_virtual_evidence(shared_variables, day):
     return vevidence, vmessage_dict
 
 
-def query_across_days(
+def query_forwardly_across_days(
     df,
     belief_propagation,
     shared_variables: List[mh.SharedVariableNode],
-    variables: List[mh.SharedVariableNode],
+    variables: List[mh.VariableNode],
     evidence_variables: List[str],
     diff_threshold,
     debug=True,
     auto_reset_shared_vars=True,
 ):
     """
+    algorithm to query the point in time model forwardly across days, thus making an approximate longitudinal inference
+
     variables: contains the names of the variables to infer, as defined in the graph
     evidence_variables: contains names of the observed variables, as defined in the df's columns
     auto_reset_shared_vars: bool to automatically reset the shared variables after the computations are done
@@ -198,6 +200,135 @@ def query_across_days(
                 )
             final_epoch = True
         epoch += 1
+
+
+def query_back_and_forth_across_days(
+    df_init,
+    belief_propagation,
+    shared_variables: List[mh.SharedVariableNode],
+    variables: List[mh.VariableNode],
+    evidence_variables: List[str],
+    diff_threshold,
+    debug=True,
+    auto_reset_shared_vars=True,
+):
+    """
+    algorithm to query the point in time model across days, thus making an approximate longitudinal inference
+    the algorithm atlernates forward and backward passes, to ensure that the day-to-day interconnected variables can propagate information backwards as efficiently as possible
+
+    variables: contains the names of the variables to infer, as defined in the graph
+    evidence_variables: contains names of the observed variables, as defined in the df's columns
+    auto_reset_shared_vars: bool to automatically reset the shared variables after the computations are done
+    """
+    df_init = df_init.reset_index(drop=True)
+    final_pass = False
+    passes = 0
+
+    # Check that each date in Date Redorded is unique
+    assert df_init["Date Recorded"].nunique() == len(
+        df_init
+    ), "Error: Cannot process input df as there are doublons in the Date Recorded column."
+
+    df_res_before_convergence = pd.DataFrame({})
+    df_res_final_epoch = pd.DataFrame({})
+
+    # Initialize posteriors distribution to uniform
+    posteriors_old = [
+        get_uniform_message(shared_var.card) for shared_var in shared_variables
+    ]
+
+    while True:
+        forward_pass = passes % 2 == 0
+        if forward_pass:
+            if debug:
+                print(f"Pass {passes} (forward)")
+            df = df_init.sort_values("Date Recorded", ascending=True).reset_index(
+                drop=True
+            )
+        else:
+            if debug:
+                print(f"Pass {passes} (backwards)")
+            df = df_init.sort_values("Date Recorded", ascending=False).reset_index(
+                drop=True
+            )
+
+        for i, row in df.iterrows():
+            day = row["Date Recorded"].strftime("%Y-%m-%d")
+
+            # Get query inputs
+            evidence_dict = build_evidence(df, i, evidence_variables)
+            vevidence, vmessage_dict = build_virtual_evidence(shared_variables, day)
+
+            if final_pass:
+                # Query all variables to get all posteriors
+                vars_to_infer = get_var_name_list(shared_variables + variables)
+                if debug:
+                    print(
+                        f"Querying all variables: {vars_to_infer} with evidence: {evidence_dict} and virtual evidence: {vmessage_dict}"
+                    )
+                query_res = belief_propagation.query(
+                    vars_to_infer, evidence_dict, vevidence
+                )
+                df_res_final_epoch = save_res_to_df(
+                    df_res_final_epoch,
+                    day,
+                    query_res,
+                    vars_to_infer,
+                    row,
+                    evidence_variables,
+                )
+            else:
+                # Query shared variables to get cross plate message
+                vars_to_infer = get_var_name_list(shared_variables)
+                if debug:
+                    print(
+                        f"Querying all variables: {vars_to_infer} with evidence: {evidence_dict} and virtual evidence: {vmessage_dict}"
+                    )
+                query_res, query_messages = belief_propagation.query(
+                    vars_to_infer, evidence_dict, vevidence, get_messages=True
+                )
+                df_res_before_convergence = save_res_to_df(
+                    df_res_before_convergence,
+                    f"{passes}, {day}",
+                    query_res,
+                    vars_to_infer,
+                    row,
+                    evidence_variables,
+                )
+                for shared_var in shared_variables:
+                    # Get newly computed message from the query output
+                    new_message = query_messages[shared_var.factor_node_key]
+                    shared_var.add_or_update_message(day, new_message)
+                    if len(vmessage_dict) == 0:
+                        vmessage = get_uniform_message(shared_var.card)
+                    else:
+                        vmessage = vmessage_dict[shared_var.name]
+                    shared_var.set_agg_virtual_message(vmessage, new_message)
+
+        posteriors_old, diffs = get_diffs(query_res, posteriors_old, shared_variables)
+
+        for shared_var, diff in zip(shared_variables, diffs):
+            if debug:
+                print(f"Pass {passes} - Posteriors' diff for {shared_var.name}: {diff}")
+
+        # Convergence reached when the diff is below the threshold
+        # or when the maximum number of passes is reached
+        # When convergence is reached, run another epoch to get all posteriors
+        if np.sum(diffs) < diff_threshold or passes > 99:
+            if final_pass:
+                if auto_reset_shared_vars:
+                    for shared_var in shared_variables:
+                        shared_var.reset()
+                return df_res_final_epoch, df_res_before_convergence, shared_variables
+            if passes % 2 == 1:
+                # Must end on a backward pass
+                if debug:
+                    print(
+                        f"Final pass - All diffs are below {diff_threshold}, running another epoch to get all posteriors"
+                    )
+
+                final_pass = True
+        passes += 1
 
 
 def plot_scatter(fig, x, y, row, col, colour=None, title=None):
@@ -307,16 +438,16 @@ def plot_posterior_validation(
         coloraxis1=dict(
             colorscale=colorscale,
             colorbar_x=1,
-            colorbar_y=0.5,
+            colorbar_y=0.43,
             colorbar_thickness=23,
-            colorbar_len=0.3,
+            colorbar_len=0.305,
         ),
         coloraxis2=dict(
             colorscale=colorscale,
             colorbar_x=1,
-            colorbar_y=0.15,
+            colorbar_y=0.14,
             colorbar_thickness=23,
-            colorbar_len=0.3,
+            colorbar_len=0.305,
         ),
     )
     if save:
@@ -432,17 +563,17 @@ def plot_query_res(
         coloraxis1=dict(
             colorscale=colorscale,
             colorbar_x=0.45,
-            colorbar_y=0.402,
+            colorbar_y=0.505,
             colorbar_thickness=23,
-            colorbar_len=0.415,
+            colorbar_len=0.34,
             colorbar={"title": "AR"},
         ),
         coloraxis2=dict(
             colorscale=colorscale,
             colorbar_x=1,
-            colorbar_y=0.402,
+            colorbar_y=0.505,
             colorbar_thickness=23,
-            colorbar_len=0.415,
+            colorbar_len=0.34,
             colorbar={"title": "IA"},
         ),
     )
