ff945290fbbce6390a0f0040bc3c50ad08d9f119
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-09-16 15:52:55 +0200

    App: add HFEV1 as observed variable + separate figure

--- src/app/components/sliders.py
+++ src/app/components/sliders.py
@@ -1,6 +1,31 @@
 import dash_bootstrap_components as dbc
 from dash import dcc
 
+hfev1_slider_layout = dbc.Form(
+    id="HFEV1-slider-container",
+    children=[
+        dbc.Label("HFEV1 observed:"),
+        dcc.Slider(
+            id="HFEV1-slider",
+            min=1,
+            max=6,
+            step=0.1,
+            value=4.5,
+            marks={
+                # 1: "1 L",
+                2: "2 L",
+                3: "3 L",
+                4: "4 L",
+                5: "5 L",
+            },
+            tooltip={
+                "placement": "bottom",
+            },
+        ),
+    ],
+    style={"display": "block"},
+)
+
 fev1_slider_layout = dbc.Form(
     id="FEV1-slider-container",
     children=[
