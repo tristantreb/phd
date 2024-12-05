4faca9e7b87743eeb9a7e7a6f0c56e62e1615792
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-12-05 15:15:41 +0000

    Cutset cond: allow long model to process different set of obs for each day

--- src/inf_cutset_conditioning/cutset_cond_algs.py
+++ src/inf_cutset_conditioning/cutset_cond_algs.py
@@ -202,7 +202,6 @@ def compute_log_p_D_given_M_for_noise_model(
 
 def run_long_noise_model_through_time(
     df,
-    type="fev1, fef2575",
     ar_prior="uniform",
     ia_prior="uniform",
     ar_change_cpt_suffix=None,
@@ -222,43 +221,43 @@ def run_long_noise_model_through_time(
         )
     )
 
-    if type == "fev1, fef2575":
-        (
-            fig,
-            p_M_given_D,
-            log_p_D_given_M,
-            AR_given_M_and_D,
-            AR_given_M_and_all_D,
-        ) = calc_log_p_D_given_M_and_AR_for_ID_obs_fev1_fef2575(
-            df,
-            inf_alg,
-            HFEV1,
-            HFEV1_obs_list,
-            AR,
-            ecFEV1,
-            ecFEF2575prctecFEV1,
-            model_spec_txt,
-            debug=debug,
-            save=save,
-        )
-    elif type == "fev1":
-        (
-            fig,
-            p_M_given_D,
-            log_p_D_given_M,
-            AR_given_M_and_D,
-            AR_given_M_and_all_D,
-        ) = calc_log_p_D_given_M_and_AR_for_ID_obs_fev1(
-            df,
-            inf_alg,
-            HFEV1,
-            HFEV1_obs_list,
-            AR,
-            ecFEV1,
-            model_spec_txt,
-            debug=debug,
-            save=save,
-        )
+    # if type == "fev1, fef2575":
+    (
+        fig,
+        p_M_given_D,
+        log_p_D_given_M,
+        AR_given_M_and_D,
+        AR_given_M_and_all_D,
+    ) = calc_log_p_D_given_M_and_AR_for_ID_any_obs(
+        df,
+        inf_alg,
+        HFEV1,
+        HFEV1_obs_list,
+        AR,
+        ecFEV1,
+        ecFEF2575prctecFEV1,
+        model_spec_txt,
+        debug=debug,
+        save=save,
+    )
+    # elif type == "fev1":
+    #     (
+    #         fig,
+    #         p_M_given_D,
+    #         log_p_D_given_M,
+    #         AR_given_M_and_D,
+    #         AR_given_M_and_all_D,
+    #     ) = calc_log_p_D_given_M_and_AR_for_ID_obs_fev1(
+    #         df,
+    #         inf_alg,
+    #         HFEV1,
+    #         HFEV1_obs_list,
+    #         AR,
+    #         ecFEV1,
+    #         model_spec_txt,
+    #         debug=debug,
+    #         save=save,
+    #     )
     return fig, p_M_given_D, log_p_D_given_M, AR_given_M_and_D, AR_given_M_and_all_D
 
 
@@ -397,6 +396,173 @@ def load_long_noise_model_through_time_light(
         ecFEF2575prctecFEV1,
         model_spec_txt,
     )
+
+
+def calc_log_p_D_given_M_and_AR_for_ID_any_obs(
+    df,
+    inf_alg,
+    HFEV1,
+    HFEV1_obs_idx_list,
+    AR,
+    ecFEV1,
+    ecFEF2575prctecFEV1,
+    model_spec_txt,
+    debug=False,
+    save=False,
+):
+    df = df.copy().sort_values(by="Date Recorded").reset_index(drop=True)
+    id = df.loc[0, "ID"]
+
+    if debug:
+        print(f"ID {id}")
+
+    N = len(df)
+    df = df.copy()
+    H = len(HFEV1_obs_idx_list)
+    log_p_D_given_M = np.zeros((N, H))
+    AR_given_M_and_past_D = np.zeros((N, AR.card, H))
+    AR_given_M_and_same_day_D = np.zeros((N, AR.card, H))
+
+    arr = np.ones(AR.card)
+    arr /= arr.sum()
+    uniform_from_o2_side = {
+        "['O2 saturation if fully functional alveoli (%)', 'Healthy O2 saturation (%)', 'Airway resistance (%)'] -> Airway resistance (%)": arr
+    }
+    uniform_from_fef2575 = {
+        "['ecFEF25-75 % ecFEV1 (%)', 'Airway resistance (%)'] -> Airway resistance (%)": arr
+    }
+    m_from_hfev1_key = "Healthy FEV1 (L) -> ['Underlying ecFEV1 (L)', 'Healthy FEV1 (L)', 'Airway resistance (%)']"
+    m_from_hfev1_dict = {}
+    m_from_fev_factor_key = "['Underlying ecFEV1 (L)', 'Healthy FEV1 (L)', 'Airway resistance (%)'] -> Airway resistance (%)"
+    m_from_fev_factor_dict = {}
+
+    # Get the joint probability of ecFEV1 and ecFEF2575 given the model for this individual
+    # Process each row
+    # P(model | data) prop_to P(data | model) * P(model)
+    # P(data | model) = P(ecFEV1, ecFEF2575 | HFEV1) = P(ecFEV1 | HFEV1) * P( ecFEF2575 | HFEV1, ecFEV1)
+    tic = time.time()
+    for n, row in df.iterrows():
+        curr_date = df.loc[n, "Date Recorded"]
+        # There is no prev day if it's the first day
+        prev_date = df.iloc[n - 1]["Date Recorded"] if n > 0 else None
+        if debug:
+            print(f"Row {n + 1}/{N}, Date: {curr_date}, Prev Date: {prev_date}")
+
+        # For each model given an HFEV1 observation
+        for h, HFEV1_bin_idx in enumerate(HFEV1_obs_idx_list):
+            vevidence_ar = cutseth.build_vevidence_cutset_conditioned_ar(
+                AR, h, curr_date, prev_date, next_date=None, debug=debug
+            )
+
+            if debug:
+                print(
+                    f"HFEV1_obs: {HFEV1_bin_idx}, vevidence_ar: {vevidence_ar.values}"
+                )
+
+            if not np.isnan(row["idx ecFEV1 (L)"]) and not np.isnan(
+                row["idx ecFEF25-75 % ecFEV1 (%)"]
+            ):
+                if debug:
+                    print("Both ecFEV1 and ecFEF25-75 observed")
+                log_p_D_given_M_for_row, dist_AR = (
+                    get_AR_and_p_log_D_given_M_obs_fev1_and_fef2575(
+                        row,
+                        inf_alg,
+                        HFEV1,
+                        HFEV1_bin_idx,
+                        ecFEV1,
+                        ecFEF2575prctecFEV1,
+                        AR,
+                        vevidence_ar,
+                        uniform_from_o2_side | uniform_from_fef2575,
+                        m_from_hfev1_dict,
+                        m_from_hfev1_key,
+                        m_from_fev_factor_dict,
+                        m_from_fev_factor_key,
+                    )
+                )
+            elif not np.isnan(row["idx ecFEV1 (L)"]) and np.isnan(
+                row["idx ecFEF25-75 % ecFEV1 (%)"]
+            ):
+                if debug:
+                    print("Only ecFEV1 observed")
+                log_p_D_given_M_for_row, dist_AR = (
+                    get_AR_and_p_log_D_given_M_obs_fev1(
+                        row,
+                        inf_alg,
+                        HFEV1,
+                        HFEV1_bin_idx,
+                        ecFEV1,
+                        AR,
+                        vevidence_ar,
+                        uniform_from_o2_side | uniform_from_fef2575,
+                        m_from_hfev1_dict,
+                        m_from_hfev1_key,
+                        m_from_fev_factor_dict,
+                        m_from_fev_factor_key,
+                    )
+                )
+            elif np.isnan(row["idx ecFEV1 (L)"]) and np.isnan(
+                row["idx ecFEF25-75 % ecFEV1 (%)"]
+            ):
+                if debug:
+                    print("No data observed")
+                log_p_D_given_M_for_row, dist_AR = (
+                    get_AR_and_p_log_D_given_M_no_obs(
+                        inf_alg,
+                        HFEV1,
+                        HFEV1_bin_idx,
+                        AR,
+                        vevidence_ar,
+                        uniform_from_o2_side | uniform_from_fef2575,
+                        m_from_hfev1_dict,
+                        m_from_hfev1_key,
+                    )
+                )
+            else:
+                raise ValueError(f"Unexpected combination of observed variables for row\n{row}")
+
+            log_p_D_given_M[n, h] = log_p_D_given_M_for_row
+            AR_given_M_and_past_D[n, :, h] = dist_AR
+
+            # Add the AR dist to be used for as next day's AR vevidence
+            date_str = row["Date Recorded"].strftime("%Y-%m-%d")
+            AR.add_or_update_posterior(h, date_str, dist_AR, debug)
+
+            # Get the AR without the contribution from the past days' data
+            # This will be used to get the contribution from the next days' data
+            # Check that wherever vevidence_ar is 0, the res2[AR.name] is also 0
+            AR_without_vevidence = np.divide(
+                dist_AR, vevidence_ar.values, where=vevidence_ar.values != 0
+            )
+            AR_without_vevidence /= AR_without_vevidence.sum()
+            AR_given_M_and_same_day_D[n, :, h] = AR_without_vevidence
+
+    toc = time.time()
+    print(f"{id} - Time for {N} entries: {toc-tic:.2f} s")
+
+    AR_given_M_and_all_D = run_backward_sweep_to_get_ar_posteriors(
+        df,
+        N,
+        H,
+        HFEV1_obs_idx_list,
+        AR,
+        AR_given_M_and_past_D,
+        AR_given_M_and_same_day_D,
+        debug,
+    )
+
+    p_M_given_D, log_p_D_given_M, AR_given_M_and_D, AR_given_M_and_all_D = (
+        fuse_results_from_conditioned_models(
+            HFEV1, H, HFEV1_obs_idx_list, log_p_D_given_M, AR_given_M_and_all_D
+        )
+    )
+
+    fig = plot_cutset_cond_results(
+        df, HFEV1, p_M_given_D, AR, AR_given_M_and_D, model_spec_txt, save
+    )
+
+    return (fig, p_M_given_D, log_p_D_given_M, AR_given_M_and_D, AR_given_M_and_all_D)
 
 
 def calc_log_p_D_given_M_and_AR_for_ID_obs_fev1_fef2575(
@@ -774,6 +940,41 @@ def get_AR_and_p_log_D_given_M_obs_fev1(
     dist_AR /= dist_AR.sum()
     return log_p_D_given_M, dist_AR
 
+
+def get_AR_and_p_log_D_given_M_no_obs(
+    bp,
+    HFEV1,
+    HFEV1_bin_idx,
+    AR,
+    vevidence_ar,
+    precomp_messages,
+    m_from_hfev1_dict,
+    m_from_hfev1_key,
+):
+    # Get P(AR | model conditionned on HFEV1_obs)
+    ref = f"{HFEV1_bin_idx}"
+    add_ref = True
+    if ref in m_from_hfev1_dict:
+        precomp_messages.update(m_from_hfev1_dict[ref])
+        add_ref = False
+    res, messages = bp.query(
+        variables=[AR.name],
+        evidence={HFEV1.name: HFEV1_bin_idx},
+        virtual_evidence=[vevidence_ar],
+        precomp_messages=precomp_messages,
+        get_messages=True,
+    )
+    if add_ref:
+        m_from_hfev1_dict.update({ref: {m_from_hfev1_key: messages[m_from_hfev1_key]}})
+
+    dist_AR = res[AR.name].values
+    dist_AR /= dist_AR.sum()
+
+    # No contribution from data on this day
+    log_D_given_M = np.nan
+    return log_D_given_M, dist_AR
+
+
 def run_backward_sweep_to_get_ar_posteriors(
     df,
     N,
@@ -836,7 +1037,8 @@ def fuse_results_from_conditioned_models(
     log_p_M_given_D = np.zeros(H)
     for h, HFEV1_bin_idx in enumerate(HFEV1_obs_idx_list):
         log_p_M_hfev1 = np.log(HFEV1.cpt[HFEV1_bin_idx])
-        log_p_M_given_D[h] = np.sum(log_p_D_given_M[:, h]) + log_p_M_hfev1
+        # Do nan sums to remove contributions from days without data
+        log_p_M_given_D[h] = np.nansum(log_p_D_given_M[:, h]) + log_p_M_hfev1
 
     # Exponentiating very negative numbers gives too small numbers
     # Setting the highest number to 1 to prevent numerical issues
