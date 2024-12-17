8f92227103845f8b640cb91465402b65e7a5a156
Tristan Trébaol <tristan.trebaol@gmail.com>
2024-12-17 17:36:08 +0100

    Compute cpt with HFEV1 starting at 0

--- src/models/var_builders.py
+++ src/models/var_builders.py
@@ -561,6 +561,132 @@ def o2sat_fev1_fef2575_point_in_time_model_noise_shared_healthy_vars(
         ecFEF2575prctecFEV1,
     )
 
+def o2sat_fev1_fef2575_point_in_time_model_noise_shared_healthy_vars_log(
+    height,
+    age,
+    sex,
+    ia_prior="uniform",
+    ar_prior="uniform",
+    ecfev1_noise_model_cpt_suffix="_std_add_mult",
+    ar_fef2575_cpt_suffix="_ecfev1_2_days_model_add_mult_noise",
+):
+    """
+    Point in time model with full FEV1, FEF25-75 and O2Sat sides
+
+    There is no factor linking AR and IA in this model. The priors for AR, IA are uniform
+    """
+    hfev1_prior = {"type": "default", "height": height, "age": age, "sex": sex}
+    ho2sat_prior = {
+        "type": "default",
+        "height": height,
+        "sex": sex,
+    }
+    HFEV1 = SharedVariableNode("Healthy FEV1 (L)", 0, 6, 0.05, prior=hfev1_prior)
+    
+    def get_prior_p_in_log(var):
+        prior = np.log(HFEV1.get_distribution_as_sample(HFEV1.cpt, 0.001))
+        hist, _ = np.histogram(
+                prior, bins=np.arange(HFEV1.a, HFEV1.b + HFEV1.bin_width / 2, HFEV1.bin_width)
+            )
+        return hist
+    
+    HFEV1.cpt = get_prior_p_in_log(HFEV1)
+
+    ecFEV1 = VariableNode("ecFEV1 (L)", 0, 6, 0.05, prior=None)
+    uecFEV1 = VariableNode("Underlying ecFEV1 (L)", 0, 6, 0.05, prior=None)
+
+    ecFEF2575prctecFEV1 = VariableNode("ecFEF25-75 % ecFEV1 (%)", 0, 200, 2, prior=None)
+    # Lowest predicted FEV1 is 15% (AR = 1-predictedFEV1)
+    AR = VariableNode("Airway resistance (%)", 0, 90, 2, prior=None)
+    if ar_prior == "uniform":
+        AR.cpt = AR.set_prior({"type": "uniform"})
+    elif ar_prior == "uniform in log space":
+        AR.cpt = AR.set_prior(
+            {"type": "custom", "p": get_uniform_prior_in_log_space(AR)}
+        )
+    elif ar_prior == "uniform message to HFEV1":
+        AR.cpt = AR.set_prior(
+            {"type": "custom", "p": get_prior_for_uniform_hfev1_message(AR)}
+        )
+    elif ar_prior == "breathe (2 days model, ecFEV1, ecFEF25-75)":
+        AR.cpt = AR.set_prior(
+            {
+                "type": "custom",
+                "p": ar.get_breathe_prior_from_2_days_model_ecFEV1_ecFEF2575(),
+            }
+        )
+    elif ar_prior == "breathe (1 day model, O2Sat, ecFEV1)":
+        AR.cpt = AR.set_prior(
+            {
+                "type": "custom",
+                "p": ar.get_breathe_prior_from_1_day_model_o2sat_ecFEV1(),
+            }
+        )
+    elif ar_prior == "breathe (2 days model, ecFEV1, ecFEF25-75, add mult noise)":
+        AR.cpt = AR.set_prior(
+            {
+                "type": "custom",
+                "p": ar.get_breathe_prior_from_2_days_model_ecFEV1_ecFEF2575_ecfev1addmultnoise(),
+            }
+        )
+
+    # Res 0.5 takes 19s, res 0.2 takes 21s
+    HO2Sat = SharedVariableNode(
+        "Healthy O2 saturation (%)", 90, 100, 0.5, prior=ho2sat_prior
+    )
+    # Highest drop is 92% (for AR = 90%)
+    # Hence the lowest O2SatFFA is 90 * 0.92 = 82.8
+    O2SatFFA = VariableNode(
+        "O2 saturation if fully functional alveoli (%)", 80, 100, 0.5, prior=None
+    )
+    # O2 sat can't be below 70%.
+    # If there's no airway resistance, it should still be possible to reach 70% O2 sat
+    # Hence, min IA is 30% because i
+    if ia_prior == "uniform":
+        prior = {"type": "uniform"}
+    elif ia_prior == "breathe":
+        prior = {"type": "custom", "p": get_IA_breathe_prior()}
+    else:
+        raise ValueError(f"ia_prior {ia_prior} not recognised")
+    IA = VariableNode("Inactive alveoli (%)", 0, 30, 1, prior=prior)
+
+    # In reality O2 sat can't be below 70%.
+    # However, the CPT should account for the fact that the lowest O2 sat is 82.8%.
+    # 82.8-30 = 52.8%
+    # TODO: should we hardcode the fact that the sum of AR and IA should not be below 70% O2 Sat?
+    UO2Sat = VariableNode("Underlying O2 saturation (%)", 50, 100, 0.5, prior=None)
+    O2Sat = VariableNode("O2 saturation (%)", 49.5, 100.5, 1, prior=None)
+
+    # Set shared vars factor to node keys.
+    # Used to aggregate messages up in longitudinal model
+    key_hfev1 = f"['{uecFEV1.name}', '{HFEV1.name}', '{AR.name}'] -> {HFEV1.name}"
+    key_ho2sat = f"['{O2SatFFA.name}', '{HO2Sat.name}', '{AR.name}'] -> {HO2Sat.name}"
+    HFEV1.set_factor_to_node_key(key_hfev1)
+    HO2Sat.set_factor_to_node_key(key_ho2sat)
+
+    # Calculate CPTs
+    ecFEV1.set_cpt(get_cpt([ecFEV1, uecFEV1], suffix=ecfev1_noise_model_cpt_suffix))
+    uecFEV1.set_cpt(get_cpt([uecFEV1, HFEV1, AR]))
+    O2SatFFA.set_cpt(get_cpt([O2SatFFA, HO2Sat, AR]))
+    UO2Sat.set_cpt(get_cpt([UO2Sat, O2SatFFA, IA]))
+    O2Sat.set_cpt(get_cpt([O2Sat, UO2Sat]))
+    ecFEF2575prctecFEV1.set_cpt(
+        get_cpt([ecFEF2575prctecFEV1, AR], suffix=ar_fef2575_cpt_suffix)
+    )
+
+    return (
+        HFEV1,
+        uecFEV1,
+        ecFEV1,
+        AR,
+        HO2Sat,
+        O2SatFFA,
+        IA,
+        UO2Sat,
+        O2Sat,
+        ecFEF2575prctecFEV1,
+    )
+
 
 def o2sat_fev1_fef2575_point_in_time_model_noise_shared_healthy_vars_light(
     height,
